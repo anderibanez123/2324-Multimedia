b08866646a2cc63aeed77d6c3633dcd7ce86e596
anderibanez123 <anderibanez123@gmail.com>
2023-10-05 11:28:52 +0200

    aa

--- 2. Jarduerak/ANDER_IBANEZ_JardueraAurreratua/app/src/main/java/com/example/ander_ibanez_jardueraaurreratua/MainActivity.java
+++ 2. Jarduerak/ANDER_IBANEZ_JardueraAurreratua/app/src/main/java/com/example/ander_ibanez_jardueraaurreratua/MainActivity.java
@@ -19,9 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
     // PAINT PARAMETROAK
     private  ImageButton paintButton;
+    private TextView paintTV;
 
     // MAPS PARAMETROAK
     private ImageButton mapsButton;
+    private TextView mapsTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Paint parametroak zehazten
         paintButton = findViewById(R.id.paintButton);
+        paintTV = findViewById(R.id.paintTV);
+
 
         // Maps paramertoak zehazten
         mapsButton = findViewById(R.id.mapButton);
+        mapsTV = findViewById(R.id.mapTV);
 
 
 
@@ -57,6 +62,27 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
+                if (irudiakTV.getVisibility() == View.VISIBLE){
+
+                    // Irudiak textuari visible aldatu
+                    irudiakTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (paintTV.getVisibility() == View.VISIBLE){
+
+                    // paint textuari visible aldatu
+                    paintTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (mapsTV.getVisibility() == View.VISIBLE){
+
+                    // maps textuari visible aldatu
+                    mapsTV.setVisibility(View.INVISIBLE);
+
+                }
+
 
             }
         });
@@ -66,15 +92,110 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (hasieraTV.getVisibility() == View.INVISIBLE){
+                if (irudiakTV.getVisibility() == View.INVISIBLE){
 
-                    // Hasiera textua visible jarri
-                    hasieraTV.setVisibility(View.VISIBLE);
+                    // Irudiak textua visible jarri
+                    irudiakTV.setVisibility(View.VISIBLE);
+
+                }
+
+                if (hasieraTV.getVisibility() == View.VISIBLE){
+
+                    // Hasiera textuari visible aldatu
+                    hasieraTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (paintTV.getVisibility() == View.VISIBLE){
+
+                    // paint textuari visible aldatu
+                    paintTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (mapsTV.getVisibility() == View.VISIBLE){
+
+                    // maps textuari visible aldatu
+                    mapsTV.setVisibility(View.INVISIBLE);
 
                 }
 
 
             }
+        });
+
+        // Paint button sakatzerakoan egin behar duena
+        paintTV.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                if (paintTV.getVisibility() == View.INVISIBLE){
+
+                    // paint textua visible jarri
+                    paintTV.setVisibility(View.VISIBLE);
+
+                }
+
+                if (hasieraTV.getVisibility() == View.VISIBLE){
+
+                    // Hasiera textuari visible aldatu
+                    hasieraTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (irudiakTV.getVisibility() == View.VISIBLE){
+
+                    // Irudiak textuari visible aldatu
+                    irudiakTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (mapsTV.getVisibility() == View.VISIBLE){
+
+                    // maps textuari visible aldatu
+                    mapsTV.setVisibility(View.INVISIBLE);
+
+                }
+
+            }
+
+        });
+
+        // Maps button sakatzerakoan egin behar duena
+        mapsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                if (mapsTV.getVisibility() == View.INVISIBLE){
+
+                    // Maps textua visible jarri
+                    mapsTV.setVisibility(View.VISIBLE);
+
+                }
+
+                if (hasieraTV.getVisibility() == View.VISIBLE){
+
+                    // Hasiera textuari visible aldatu
+                    hasieraTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (irudiakTV.getVisibility() == View.VISIBLE){
+
+                    // Irudiak textuari visible aldatu
+                    irudiakTV.setVisibility(View.INVISIBLE);
+
+                }
+
+                if (paintTV.getVisibility() == View.VISIBLE){
+
+                    // Paint textuari visible aldatu
+                    paintTV.setVisibility(View.INVISIBLE);
+
+                }
+
+            }
+
         });
 
 
